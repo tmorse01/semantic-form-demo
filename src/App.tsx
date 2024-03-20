c5488be455529133c38e761d70b44d937ee968f6
tmorse01 <74744091+tmorse01@users.noreply.github.com>
2024-03-19 23:35:52 -0700

    css updates

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
     <div className="app-layout">
       <h1>Semantic UI</h1>
       <h2>Demo Form</h2>
-      <MyForm />
+      <MyForm className="my-form" />
     </div>
   );
 };
